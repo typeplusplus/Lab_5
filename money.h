144367fabadd7a50b6190f41a63dca955e2f8a04
turn.exe <80058678+typeplusplus@users.noreply.github.com>
2021-10-13 22:31:53 -0700

    Update money.h

--- money.h
+++ money.h
@@ -13,14 +13,25 @@ class Money
         Money(int, int);
 
         //Mutators
+        
         //Accessors
         int getDollars() const;
         int getCents() const;
         double getTotal() const;
 
         friend ostream& operator <<(ostream& out, Money a);
+        
         friend const Money operator +(const Money & value1, const Money & value2);
-
+        
+        friend const Money operator -(const Money & value1, const Money & value2);
+        
+        friend const Money operator *(const Money & value1, double amount);
+        
+        friend const Money operator *(double amount, const Money & value1);
+        
+        friend const Money operator /(const Money & value1, double amount);
+        
+        friend const bool operator ==(const Money & value1, const Money & value2);
 
     private:
         //Member data
